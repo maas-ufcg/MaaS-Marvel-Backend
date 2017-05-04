39cca2f205181429d774bedbb62300cb5732de42
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2017-05-04 14:05:59 -0300

    Remove config/main.js

--- config/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'secret': '**',
-  'database': 'mongodb://localhost:maas-marvel/maas-marvel',
-  'MARVEL_API_BASE_URL': 'http://gateway.marvel.com/v1/public',
-  'MARVEL_API_PUBLIC_KEY': '**',
-  'MARVEL_API_PRIVATE_KEY': '**',
-  'MARVEL_API_TS': '1'
-};
