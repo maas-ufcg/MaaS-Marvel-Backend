6460f4341e943b409214781beaf8546ff678f23d
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2017-05-05 23:46:06 -0300

    Fixing typos

--- app/routes/hero_routes.js
+++ app/routes/hero_routes.js
@@ -8,15 +8,15 @@ module.exports = function(app) {
   var apiRoutes = express.Router();
 
   apiRoutes.get('/', passport.authenticate('jwt', { session: false }), function(req, res) {
-    Hero.find({}, {_id: 0, __v: 0}, function(err, heros) {
+    Hero.find({}, {_id: 0, __v: 0}, function(err, heroes) {
         if(err) {
             res.json(err);
         } else {
-            res.json(heros);
+            res.json(heroes);
         }
     }).skip(parseInt(req.query['offset'] || 0)).limit(parseInt(req.query['limit'] || 20));
   });
 
   // Set url for API group routes
-  app.use('/api/heros', apiRoutes);
+  app.use('/api/heroes', apiRoutes);
 };
