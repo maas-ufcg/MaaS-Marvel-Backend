3ed80daaee9e6f935274e931df70c476a65a0674
Ygor Santos <ygor.gsan@gmail.com>
2017-05-09 16:59:07 -0300

    merge

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   'secret': '**',
   'database': 'mongodb://localhost:maas-marvel/maas-marvel',
   'MARVEL_API_BASE_URL': 'http://gateway.marvel.com/v1/public',
-  'MARVEL_API_PUBLIC_KEY': '3b6d2c2e6eb3864200ced086815588be',
-  'MARVEL_API_PRIVATE_KEY': 'c8227c91e7385175a862858b3dfc009387971ef0',
+  'MARVEL_API_PUBLIC_KEY': '**',
+  'MARVEL_API_PRIVATE_KEY': '**',
   'MARVEL_API_TS': '1'
 };
